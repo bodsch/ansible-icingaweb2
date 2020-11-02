53e2a12f7e9ac2d4481a3b864613b2d4f0c11e88
Bo.Sch <bodo@boone-schulz.de>
2020-11-02 11:37:02 +0100

    Feature/remove shell dependency (#5)
    * add new filter to create password hashed and reduce shell dependency
    * add new module icingaweb_database_user
    * fix php version detection

---
+++ filter_plugins/icingaweb_passwordhash.py
@@ -0,0 +1,60 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.utils.display import Display
+
+import json
+import crypt
+
+# https://docs.ansible.com/ansible/latest/dev_guide/developing_plugins.html
+# https://blog.oddbit.com/post/2019-04-25-writing-ansible-filter-plugins/
+
+display = Display()
+
+
+class FilterModule(object):
+    """
+        Ansible file jinja2 tests
+    """
+
+    def filters(self):
+        return {
+            'create_password_hash': self.password_hash,
+        }
+
+    def password_hash(self, data):
+        """
+
+        """
+        password_hash = ''
+
+        count = len(data.keys())
+        type_ = type(data.keys())
+
+        display.vv("found: {} ({}) entries".format(count, type_))
+        display.vvv(json.dumps(data, indent=2, sort_keys=False))
+
+        if(count != 0):
+            for key in data.keys():
+                password = data.get(key, {}).get('password')
+
+                if(password):
+                    password_hash = self.__password_hash(password)
+
+                    _ = data[key].pop('password')
+
+                data[key]['hashed'] = password_hash
+
+        display.vvv(json.dumps(data, indent=2, sort_keys=False))
+
+        with open("/tmp/icingaweb2_users.json", 'w') as fp:
+            json.dump(data, fp)
+
+        return data
+
+    # https://docs.python.org/3/library/crypt.html
+    def __password_hash(sef, plaintext):
+        result = crypt.crypt(plaintext, crypt.METHOD_MD5)
+
+        return result
---
+++ library/icingaweb_module.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2020, Bodo Schulz <bodo@boone-schulz.de>
+# BSD 2-clause (see LICENSE or https://opensource.org/licenses/BSD-2-Clause)
+
+from __future__ import absolute_import, division, print_function
+import os
+
+from ansible.module_utils.basic import AnsibleModule
+
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {
+    'metadata_version': '0.1',
+    'status': ['preview'],
+    'supported_by': 'community'
+}
+
+
+class IcingaWeb2Modules(object):
+    """
+    Main Class to implement the Icinga2 API Client
+    """
+    module = None
+
+    def __init__(self):
+        """
+          Initialize all needed Variables
+        """
+        self.state = module.params.get("state")
+        self.module = module.params.get("module")
+        self.module_path = module.params.get("module_path")
+
+    def run(self):
+        res = dict(
+            changed=False,
+            failed=False,
+            ansible_module_results="none"
+        )
+
+        module.log(msg="Module: {} - {}".format(self.module, self.state))
+
+        if(os.path.isdir(self.module_path)):
+            """
+
+            """
+            source = os.path.join(self.module_path, self.module)
+            destination = os.path.join('/etc/icingaweb2/enabledModules', self.module)
+
+            if(os.path.isdir(source)):
+
+                module.log(msg="module {} exists".format(self.module))
+
+                if(self.state == 'present'):
+                    """
+                      create link from '/usr/share/icingaweb2/modules/$MODULE' to '/etc/icingaweb2/enabledModules/$MODULE'
+                    """
+                    if(os.path.islink(destination) and os.readlink(destination) == source):
+                        #                        module.log(msg="link exists and is valid")
+                        pass
+                    else:
+                        if(not os.path.islink(destination)):
+                            self.create_link(source, destination)
+                        else:
+                            if(os.readlink(destination) != source):
+                                module.log(msg="path '{}' is a broken symlink".format(destination))
+                                self.create_link(source, destination, True)
+                            else:
+                                self.create_link(source, destination)
+
+                        res['changed'] = True
+
+                else:
+                    if(os.path.islink(destination)):
+                        os.remove(destination)
+                        res['changed'] = True
+        else:
+            msg = "{} is no directory".format(self.module_path)
+
+            res['ansible_module_results'] = msg
+            res['failed'] = True
+            module.log(msg=msg)
+
+        return res
+
+    def create_link(self, source, destination, force=False):
+        module.log(msg="create_link({}, {}, {})".format(source, destination, force))
+
+        if(force):
+            os.remove(destination)
+            os.symlink(source, destination)
+        else:
+            os.symlink(source, destination)
+
+        pass
+
+
+# ===========================================
+# Module execution.
+#
+
+
+def main():
+    global module
+    module = AnsibleModule(
+        argument_spec=dict(
+            state=dict(default="present", choices=["absent", "present"]),
+            module=dict(required=True),
+            module_path=dict(required=False, default='/usr/share/icingaweb2/modules'),
+        ),
+        supports_check_mode=False,
+    )
+
+    icingaweb = IcingaWeb2Modules()
+    result = icingaweb.run()
+
+    module.exit_json(**result)
+
+
+# import module snippets
+if __name__ == '__main__':
+    main()
